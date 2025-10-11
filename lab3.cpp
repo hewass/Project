7b2948e0e8996b8ea4ae33cd5ec955de3a0f5289
hewass <demolite12@gmail.com>
2025-10-11 09:17:04 +0300

    switch

--- lab3.cpp
+++ lab3.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
+    int month;
+    cin >> month;
+
+}
+   #include <iostream>
+using namespace std;
+
+int main() 
+{
     int month;
     cin >> month;
 
@@ -9,24 +19,15 @@ int main() {
         case 12:
         case 1:
         case 2:
-            cout << "Зима" << endl;
+            cout << "winter" << endl;
             break;
         case 3:
         case 4:
         case 5:
-            cout << "Весна" << endl;
+            cout << "spring" << endl;
             break;
-        case 6:
-        case 7:
-        case 8:
-            cout << "Літо" << endl;
-            break;
-        case 9:
-        case 10:
-        case 11:
-            cout << "Осінь" << endl;
-            break;
-        
+       
+    }
     return 0;
 }
    
