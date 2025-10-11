b1fe9175c3fa11b47a5701019f51387d086f03f6
hewass <demolite12@gmail.com>
2025-10-11 09:17:41 +0300

    complete switch

--- lab3.cpp
+++ lab3.cpp
@@ -26,7 +26,16 @@ int main()
         case 5:
             cout << "spring" << endl;
             break;
-       
+        case 6:
+        case 7:
+        case 8:
+            cout << "summer" << endl;
+            break;
+        case 9:
+        case 10:
+        case 11:
+            cout << "autumn" << endl;
+            break;
     }
     return 0;
 }
