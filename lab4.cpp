5b831303d03d6cb37138323f4c02be7accd8eba8
hewass <demolite12@gmail.com>
2025-10-11 10:36:41 +0300

    if else

--- lab4.cpp
+++ lab4.cpp
@@ -5,6 +5,13 @@ int main()
 {
     double x,y;
     cin >> x >> y;
-    
+    if (y==1 && -2<x && x<2)
+        cout <<"YES";
+    else if (y==-1 && -2<x && x<2)
+        cout <<"YES";
+    else if (x==0 && y==0)
+        cout <<"YES";
+    else
+        cout <<"NO";
     return 0;
 }
