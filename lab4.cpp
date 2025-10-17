4b856a680caf231e44e0dd171be7f7e26c9dea9a
hewass <demolite12@gmail.com>
2025-10-17 16:04:49 +0300

    bool up down

--- lab4.cpp
+++ lab4.cpp
@@ -6,7 +6,9 @@ int main() {
     float x, y;
     cin >> x >> y;
 
+    bool up = (y >= 0 && y <= 1 && y >= fabs(x));
+    bool down = (y <= 0 && y >= -1 && y <= -fabs(x));
 
-
+  
     return 0;
 }
