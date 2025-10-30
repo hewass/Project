cfe1718d86131f734e7787035f174c0a42d3fc19
hewass <demolite12@gmail.com>
2025-10-30 18:59:56 +0200

    while

--- lab5.cpp
+++ lab5.cpp
@@ -9,6 +9,21 @@ int main() {
     int i = 1;
     double d = 1.0;
 
+    while (i <= 15) {
+        double num = pow(-1.0, i + 5) * (6.0 * i * i + 1.0);
+        double den = 7.0 * i * i + 2.0 * i + 4.0;
+        double term = (num / den);
+
+        S += term;
+        d *= term;
+
+        cout << "n = " << i << " a = " << fixed << setprecision(4) << S << endl;
+        
+        i++;
+      
+    }
+    
+    
 
     return 0;
 }
