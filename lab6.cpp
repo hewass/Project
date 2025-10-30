0ced49ab5d643fd682b8a9481cb47a8c3cf849f6
hewass <demolite12@gmail.com>
2025-10-30 19:27:38 +0200

    double solve

--- lab6.cpp
+++ lab6.cpp
@@ -5,3 +5,26 @@
 
 using namespace std;
 
+double solve() {
+    const int n = 15; 
+    vector<double> a(n);
+    
+    a[0] = 1.0;
+    a[1] = 1.0;
+
+    int i = 2;
+    while (i < n) { 
+        a[i] = a[i-2] + a[i-1] / pow(2.0, i - 1);
+        i++;
+    }
+    
+    double product = 1.0;
+    int k = 0;
+    while (k < n) {
+        product *= a[k];
+        k++;
+    }
+    
+    return product;
+}
+
